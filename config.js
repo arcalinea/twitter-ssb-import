b17ed82f71a9d6dd5fd75ed68edf42cbc157d531
Jay Graber <jay@z.cash>
2018-05-20 08:22:23 -0700

    Default dry-run

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // config.js
 const twitterConfig = {
-    retweets: true,
+    retweets: false,
     screen_name: 'arcalinea',
     from: ['2018', '02'],
     to: ['2018', '03'],
-    dry_run: false
+    dry_run: true
 };
 
 module.exports = twitterConfig;
